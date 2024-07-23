76e26b0a0aec628e99cdbc4f13676d6830139f50
haririamir <haririamir96@gmail.com>
2024-07-23 16:17:35 +0330

    add animation

--- src/app/features/exercise/exercise.component.ts
+++ src/app/features/exercise/exercise.component.ts
@@ -3,11 +3,48 @@ import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { IExersice } from 'src/app/types/exercise/exersice.model';
 import { ExerciseService } from '../services/exercise.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-exercise',
   templateUrl: './exercise.component.html',
   styleUrls: ['./exercise.component.css'],
+  animations: [
+    trigger('list1', [
+      state(
+        'enter',
+        style({
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void=> *', [
+        style({
+          transform: 'translateX(-100px)',
+        }),
+        animate(500),
+      ]),
+    ]),
+    trigger('list2', [
+      state(
+        'exit',
+        style({
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('* <=> void', [
+        style({
+          transform: 'translateX(-100px)',
+        }),
+        animate(500),
+      ]),
+    ]),
+  ],
 })
 export class ExerciseComponent implements OnInit, OnDestroy {
   constructor(private exerciseService: ExerciseService) {}

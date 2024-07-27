fdeb51457750972067250525fc7a8766e9287e0b
haririamir <haririamir96@gmail.com>
2024-07-28 01:08:36 +0330

    fox

--- src/app/features/services/plan.service.ts
+++ src/app/features/services/plan.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { IPlan } from 'src/app/types/exercise/plan.model';
 import { ExerciseService } from './exercise.service';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from 'src/app/services/data.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PlanService {
+export class PlanService extends DataService {
   plans: BehaviorSubject<IPlan[]> = new BehaviorSubject<IPlan[]>([]);
 
-  constructor(private exService: ExerciseService) {}
-
+  constructor(http: HttpClient) {
+    super(http, 'http://localhost:3000/api/exercises');
+  }
+  
   addPlan(plan: IPlan) {
     this.plans.next([plan]);
   }
